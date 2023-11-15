4a91e9195850a6aafab5241ca0537f2189b0cf78
sushma070120 <sushmarudhramoorthi694@gmail.com>
2023-11-15 20:12:41 +0530

    calculating

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator
+{
+    static class Operations {
+        public static Integer add(Integer a, Integer b) {
+            return (a + b);
+        }
+
+        public static Integer multiply(Integer a, Integer b) {
+            return (a * b);
+        }
+    }
+}
